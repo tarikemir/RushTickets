9ebd11ca230f238e622b4ce19e3dba7ba817198c
tarikemir <81103446+tarikemir@users.noreply.github.com>
2023-12-13 22:52:17 +0300

    Contexes created for User Identity

---
+++ Infrastructure/RushTickets.Persistence/Contexts/RushTicketsIdentityContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using RushTickets.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushTickets.Persistence.Contexts
+{
+    public class RushTicketsIdentityContext : IdentityDbContext<User, Role, Guid>
+    {
+        public RushTicketsIdentityContext(DbContextOptions<RushTicketsIdentityContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //modelBuilder.Ignore<Student>();
+            //modelBuilder.Ignore<Product>();
+            //modelBuilder.Ignore<Category>();
+            //modelBuilder.Ignore<ProductCategory>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
